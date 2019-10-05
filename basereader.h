1a942c06f58af3340186d8c4565bf98cfc5728f6
stsolovey <st.solovey@gmail.com>
2019-10-05 21:37:09 +0300

    добавил basereader

---
+++ basereader.h
@@ -0,0 +1,27 @@
+#ifndef BASEREADER_H
+#define BASEREADER_H
+
+#include <QObject>
+#include <QFile>
+
+class BaseReader : public QObject
+{
+    Q_OBJECT
+public:
+    explicit BaseReader();
+    virtual ~BaseReader();
+    Q_INVOKABLE
+    void loadBase();
+    Q_INVOKABLE
+    void getNextRecord();
+    Q_INVOKABLE
+    void writeNewInformation(QString, QString);
+private:
+    QFile *file;
+signals:
+    void initEnd(bool succed);
+    void loadTown(QString town, QString info);
+private slots:
+};
+
+#endif // BASEREADER_H
