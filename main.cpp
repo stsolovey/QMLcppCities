b1f44d45236b2f8ad615e136421615d0c717a7ff
stsolovey <st.solovey@gmail.com>
2019-10-05 21:55:15 +0300

    Зарегистрировал класс basereader для qml

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "basereader.h"
 
 int main(int argc, char *argv[])
 {
@@ -15,6 +16,9 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
+
+
+    qmlRegisterType<BaseReader>("com.vv.exampleclass", 1, 0, "BaseReader");
 
     return app.exec();
 }
